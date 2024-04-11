d607a64fb972afe8d95044450facc381580e9240
MalaakJaludi <121072340+Malaak12@users.noreply.github.com>
2024-04-11 15:33:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function askQuestion() {
+    const question = document.getElementById('question').value;
+
+    // Here you can implement code to interact with ChatGPT-4 and get the answer
+    // For simplicity, let's just display a static answer
+    const answer = "This is a placeholder answer.";
+
+    document.getElementById('answer').innerText = answer;
+}
